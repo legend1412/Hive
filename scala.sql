817cf26f0cf8dde1cc07a54e646e42e70c5fc5af
legend1412 <zhaojianhao_1984@163.com>
2019-12-14 15:20:17 +0800

    office

--- scala.sql
+++ scala.sql
@@ -26,14 +26,22 @@ _.split(" ")
 
 --1、对a中每一行进行提取单词
 a.map(x=>x.split(" "))
+
 a.map(_.split(" "))
 res5: List[Array[String]] = List(Array(Looking, back, on, the, Victorian, era), 
 	Array(some, sort, pictured, in), Array(jumped, out, of, a, frying-pan, into, a, fire))
+
 a.map(_.split(" ")).flatten
 res7: List[String] = List(Looking, back, on, the, Victorian, era, some, sort, pictured, in, jumped, out, of, a, frying-pan, into, a, fire)
+
 a.flatMap(_.split(" "))
 res8: List[String] = List(Looking, back, on, the, Victorian, era, some, sort, pictured, in, jumped, out, of, a, frying-pan, into, a, fire)
+
 a.flatMap(_.split(" ")).map(x=>(x,1))
 res10: List[(String, Int)] = List((Looking,1), (back,1), (on,1), (the,1), (Victorian,1), (era,1), (some,1), (sort,1), (pictured,1), (in,1), (jumped,1), (out,1), (of,1), (a,1), (frying-pan,1), (into,1), (a,1), (fire,1))
+
 a.flatMap(_.split(" ")).map(x=>(x,1)).groupBy(x=>x._1)
 res11: scala.collection.immutable.Map[String,List[(String, Int)]] = Map(in -> List((in,1)), jumped -> List((jumped,1)), fire -> List((fire,1)), Looking -> List((Looking,1)), a -> List((a,1), (a,1)), out -> List((out,1)), frying-pan -> List((frying-pan,1)), on -> List((on,1)), pictured -> List((pictured,1)), back -> List((back,1)), era -> List((era,1)), into -> List((into,1)), Victorian -> List((Victorian,1)), sort -> List((sort,1)), some -> List((some,1)), of -> List((of,1)), the -> List((the,1)))
+
+--未写代码
+a.flatMap(_.split(" ")).map(x=>(x,1)).groupBy(x=>x._1).mapValues(x=>x.size)
